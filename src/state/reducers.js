e1ad98254fd7243cccbecfd9c54e301e4555f1e2
lucas iscovici <lucas.iscovici@moonhealth.ai>
2020-12-23 16:13:54 +0100

    remove reducers

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,29 +1,29 @@
-// import {combineReducers} from '@/redox';
+// // import {combineReducers} from '@/redox';
 
-import {auth_reducer, auth_reducer_name} from '@/auth';
+// import {auth_reducer, auth_reducer_name} from '@/auth';
 
-import {me} from './me/slice';
+// import {me} from './me/slice';
 
-// export default combineReducers(
-//     {
-//         initialState: {
-//             data: {},
-//         },
-//         reducers: {
-//             me,
-//         },
+// // export default combineReducers(
+// //     {
+// //         initialState: {
+// //             data: {},
+// //         },
+// //         reducers: {
+// //             me,
+// //         },
+// //     },
+// //     {[auth_reducer_name]: auth_reducer},
+// // );
+
+// export default {
+//     initialState: {
+//         data: {},
 //     },
-//     {[auth_reducer_name]: auth_reducer},
-// );
-
-export default {
-    initialState: {
-        data: {},
-    },
-    reducers: {
-        me,
-    },
-    pureReducers: {
-        [auth_reducer_name]: auth_reducer,
-    },
-};
+//     reducers: {
+//         me,
+//     },
+//     pureReducers: {
+//         [auth_reducer_name]: auth_reducer,
+//     },
+// };
