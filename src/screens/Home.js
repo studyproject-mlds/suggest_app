73a4393d545218e73a2132682833d7eb1ca5dca4
lucas iscovici <lucas.iscovici@moonhealth.ai>
2020-12-23 16:18:16 +0100

    use selector getDefaultWorld

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,20 +10,19 @@ import {selectors as meSelectors} from '@/state/me';
 export default function Home() {
     const {
         auth: {logout},
-        redux: {useSelector},
+        // redux: {useSelector},
     } = useApp();
-    const me = useSelector(meSelectors.getMe);
-
-    // React.useEffect(() => {
-    //     dispatch(meActions.getMe);
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
-    console.log(me);
+    const me = meSelectors.getMe();
+    const defaultWorld = meSelectors.getDefaultWorld();
 
     return (
         <Box alignItems="center" justifyContent="center" flex={1}>
             <Text>Open up App.js to start working on your app!</Text>
-            {me && me.first_name && <Text>HELLO {me.first_name}</Text>}
+            {me && me.first_name && (
+                <Text>
+                    HELLO {me.first_name} {defaultWorld}
+                </Text>
+            )}
             <Button
                 onPress={() => {
                     logout();
