246c86b7df64a8ee29554f7230d60b5400a468c3
lucas iscovici <lucas.iscovici@moonhealth.ai>
2020-12-23 16:12:40 +0100

    add props for button componeent

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,32 +6,35 @@ import Text from './Text';
 import {createRestyleComponent, createVariant} from '@shopify/restyle';
 import {height, width} from '@/theme';
 
-export const Button = ({onPress, label, children, ...rest}: Props) => {
-  const variant = createVariant({themeKey: 'buttonVariants'});
-  const props = useRestyle(
-    [variant, spacing, border, backgroundColor, height, width],
-    rest,
-  );
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <View {...props}>
-        {!children && <Text>{label}</Text>}
-        {children && children}
-      </View>
-    </TouchableOpacity>
-  );
+export const Button = ({
+    onPress,
+    label,
+    children,
+    style,
+    textProps = {},
+    ...rest
+}) => {
+    const variant = createVariant({themeKey: 'buttonVariants'});
+    const props = useRestyle(
+        [variant, spacing, border, backgroundColor, height, width],
+        rest,
+    );
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <View {...props}>
+                {!children && <Text {...textProps}>{label}</Text>}
+                {children && children}
+            </View>
+        </TouchableOpacity>
+    );
 };
 
-import styled from 'styled-components/native';
-
-const RoundedButtonStyled = styled(Button)`
-  border-radius: ;
-`;
-
-export const RoundedButton = ({onPress, label, children, ...rest}: Props) => {
-  return (
-    <RoundedButtonStyled {...{onPress, label, children, ...rest}}>
-      {children}
-    </RoundedButtonStyled>
-  );
+export const RoundedButton = ({onPress, label, children, radius, ...rest}) => {
+    return (
+        <Button
+            style={{borderRadius: radius}}
+            {...{onPress, label, children, ...rest}}>
+            {children}
+        </Button>
+    );
 };
