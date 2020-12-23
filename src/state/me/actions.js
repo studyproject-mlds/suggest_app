9fb56c022bb0e3ce31761a6aec5a44a1a830b7bd
lucas iscovici <lucas.iscovici@moonhealth.ai>
2020-12-23 16:13:19 +0100

    add actions for me with catch

--- src/state/me/actions.js
+++ src/state/me/actions.js
@@ -1,4 +1,24 @@
 export const getMe = async ({api}) => {
-  const res = await api.get('me/');
-  return res.data;
+    const res = await api.get('me/');
+
+    return res.data;
+};
+
+export const setDefaultWorld = async ({api, world}) => {
+    try {
+        await api.patch('me/set_default_world/', {world});
+    } catch (err) {
+        const {response = {}} = err;
+        if (
+            response?.status === 400 &&
+            (response?.data?.error ?? '').includes('bad world name')
+        ) {
+            err.message = 'World Name invalid';
+        } else {
+            err.message = 'pb pour set le world';
+        }
+
+        throw err;
+    }
+    return world;
 };
