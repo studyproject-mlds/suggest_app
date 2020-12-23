eb24e1f7a73e882f9b3f4c7bb5b530649e700585
lucas iscovici <lucas.iscovici@moonhealth.ai>
2020-12-23 16:13:36 +0100

    refactor slice for me

--- src/state/me/slice.js
+++ src/state/me/slice.js
@@ -1,21 +1,32 @@
-import {getMe} from './actions';
+import {getMe, setDefaultWorld} from './actions';
 
 export const me = () => ({
-    // noPrefix: false,
-    selectors: {
-        // getMe: 'me.data.me[0]', // prevent undefined path -> permit to choice default value here
-        getMe: (state) => state?.me?.data?.me?.[0] ?? {},
-        getDefaultWorld: (state, selectors) =>
-            selectors.getMe(state)?.default_world ?? null,
-    },
     initialState: {
         data: {
-            me: {},
+            default_world_name: null,
         },
     },
-    // user: {
-    [getMe.name]: (state, {payload}) => {
-        state.data.me = payload;
+    selectors: {
+        //     // getMe: 'me.data.me[0]', // prevent undefined path -> permit to choice default value here
+        getMe: (state) => state?.data ?? {},
+        getDefaultWorld: (state, selectors) =>
+            selectors.getMe(state)?.default_world_name ?? null,
     },
+    // getter : {
+    //     getDefaultWorld(me){
+
+    //     }
     // },
+    [getMe.name]: (state, {payload}) => {
+        state.data = payload?.[0] ?? {};
+    },
+    [setDefaultWorld.name]: (state, {payload}) => {
+        state.data.default_world_name = payload;
+    },
 });
+
+//TODO: thincking about accessor (get)
+// const draftSafeSelector = createDraftSafeSelector(
+//   selectSelf,
+//   state => state.value
+// )
