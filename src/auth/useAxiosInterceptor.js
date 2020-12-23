1b33dc25c31aa77d454570dcb9e16a9c7cb6accd
lucas iscovici <lucas.iscovici@moonhealth.ai>
2020-12-23 16:11:27 +0100

    correct axiosInterceptor
    - (400 when invalid_grant text)
    - request config only when data is not undefined

--- src/auth/useAxiosInterceptor.js
+++ src/auth/useAxiosInterceptor.js
@@ -5,47 +5,69 @@ import {deleteAccessToken} from './auth-state.js';
 
 import useAuth from './useAuth';
 const useAxiosInterceptor = () => {
-  const dispatch = useDispatch();
-  const {getAccessTokenSilently} = useAuth();
+    const dispatch = useDispatch();
+    const {getAccessTokenSilently} = useAuth();
 
-  const axiosInterceptor = useCallback(
-    (axios_instance_original) => {
-      if (axios_instance_original) {
-        axios_instance_original.interceptors.response.use(
-          async (response) => {
-            if ([400, 401].includes(response.status)) {
-              await dispatch(deleteAccessToken());
-              await getAccessTokenSilently();
-            }
-            return response;
-          },
-          async (error) => {
-            const {response} = error;
+    const axiosInterceptor = useCallback(
+        (axios_instance_original) => {
+            if (axios_instance_original) {
+                axios_instance_original.interceptors.response.use(
+                    async (response) => {
+                        if ([400, 401].includes(response?.status)) {
+                            if (
+                                response?.status === 400 &&
+                                response?.data?.error !== 'invalid_grant'
+                            ) {
+                                return response;
+                            }
+                            await dispatch(deleteAccessToken());
+                            await getAccessTokenSilently();
+                        }
+                        return response;
+                    },
+                    async (error) => {
+                        const {response, request} = error;
 
-            if ([400, 401].includes(response.status)) {
-              await dispatch(deleteAccessToken());
-              await getAccessTokenSilently({authState: {accessToken: null}});
+                        if ([400, 401].includes(response?.status)) {
+                            if (
+                                response?.status === 400 &&
+                                response?.data?.error !== 'invalid_grant'
+                            ) {
+                                throw error;
+                                // return;
+                            }
+                            try {
+                                await dispatch(deleteAccessToken());
+                                await getAccessTokenSilently({
+                                    authState: {accessToken: null},
+                                });
+                                throw new Error('AUTH: RETRY');
+                            } catch (err) {
+                                throw err;
+                            }
+                        }
+                        throw error;
+                    },
+                );
+                // Intercept all request
+                axios_instance_original.interceptors.request.use(
+                    (config) => {
+                        // console.log('cc', config.data);
+                        config.data = config.data !== undefined && {
+                            ...axios_instance_original.defaults.data,
+                            ...config.data,
+                        };
+
+                        return config;
+                    },
+                    (error) => Promise.reject(error),
+                );
             }
-            throw error;
-          },
-        );
-        // Intercept all request
-        axios_instance_original.interceptors.request.use(
-          (config) => {
-            config.data = {
-              ...axios_instance_original.defaults.data,
-              ...config.data,
-            };
-            return config;
-          },
-          (error) => Promise.reject(error),
-        );
-      }
-      return axios_instance_original;
-    },
-    [dispatch, getAccessTokenSilently],
-  );
-  return axiosInterceptor;
+            return axios_instance_original;
+        },
+        [dispatch, getAccessTokenSilently],
+    );
+    return axiosInterceptor;
 };
 
 export default useAxiosInterceptor;
