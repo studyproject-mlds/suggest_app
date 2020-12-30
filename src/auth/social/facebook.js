f0394fa871ef1ff06569e20a1af997ff47303661
lucas iscovici <lucas.iscovici@moonhealth.ai>
2020-12-30 19:32:40 +0100

    send token in object with accessToken key

--- src/auth/social/facebook.js
+++ src/auth/social/facebook.js
@@ -2,30 +2,29 @@ import {AccessToken} from 'react-native-fbsdk';
 import {LoginManager} from 'react-native-fbsdk';
 
 export const login = ({
-  errorCB = () => {},
-  callback = () => {},
-  ...props
+    errorCB = () => {},
+    callback = () => {},
+    ...props
 } = {}) => {
-  LoginManager.logInWithPermissions(['public_profile']).then(
-    function (result) {
-      if (result.isCancelled) {
-        throw new Error('fb.cancelled');
-      } else {
-        AccessToken.getCurrentAccessToken().then((data) => {
-          console.log(data.accessToken.toString());
-          callback(data.accessToken.toString());
-        });
-      }
-    },
-    function (error) {
-      errorCB(error);
-      console.log('Login fail with error: ' + error);
-    },
-  );
+    LoginManager.logInWithPermissions(['public_profile']).then(
+        function (result) {
+            if (result.isCancelled) {
+                throw new Error('fb.cancelled');
+            } else {
+                AccessToken.getCurrentAccessToken().then((data) => {
+                    callback({accessToken: data.accessToken.toString()});
+                });
+            }
+        },
+        function (error) {
+            errorCB(error);
+            console.log('Login fail with error: ' + error);
+        },
+    );
 };
 
 export const logout = () => {
-  LoginManager.logOut();
+    LoginManager.logOut();
 };
 
 export const backend = 'facebook';
